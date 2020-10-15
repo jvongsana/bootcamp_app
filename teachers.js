9521156c49a59c63fbbea97c6c3a4f28bc97a11c
Jason Vongsana <jvongsana@hotmail.com>
2020-10-15 02:37:19 +0000

    parameterizing queries

--- teachers.js
+++ teachers.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-const myArgs = process.argv.slice(2)
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -12,16 +13,17 @@ pool.connect(() => {
   console.log('connected');
 });
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${myArgs[0]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
